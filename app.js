d242a44b5bcc5a35d4cf7c72385e1d17b08af541
m19 <martijn@m19.nl>
2014-06-22 20:54:20 +0200

    Added change master password functionality

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var requestRunning = false;
 
 $(document).ready(function () {
-    var step = 1, public_key, secretPhrase, tx_to, nm_amount, nm_fee, master_salt, encrypted_wallet, master_password, adr = new NxtAddress();
+    var sendNxtStep = 1, public_key, secretPhrase, tx_to, nm_amount, nm_fee, master_salt, encrypted_wallet, master_password, adr = new NxtAddress();
 
     /**
      * Refresh Balance
      */
     function refresh_balance() {
-        $("#balance").html('<img src="./../img/ajax-loader_small.gif">')
+        $("#balance").html('<img src="./img/ajax-loader_small.gif">')
         $("#balance_additional").hide()
 
         $.post("/get_balance.php", { i: $("#own_account_id").text() }, function (data) {
@@ -35,7 +35,7 @@ $(document).ready(function () {
         /**
          * Get wallet
          */
-        if (step === 1) {
+        if (sendNxtStep === 1) {
 
             tx_to = $("#tx_to").val();
             nm_amount = $("#nm_amount").val();
@@ -51,7 +51,7 @@ $(document).ready(function () {
             }
 
             requestRunning = true;
-            $(".loading").html('Please wait <img src="./../img/ajax-loader.gif">').show();
+            $(".loading").html('<img src="./img/ajax-loader.gif">').show();
 
 
             adr.set(tx_to);
@@ -89,7 +89,7 @@ $(document).ready(function () {
                     $("#body_message").html(message_html + password_div).show();
                     $("#footer_message").html(footer_message_html).show();
 
-                    step = 2;
+                    sendNxtStep = 2;
                 } else {
 
                     $(".loading").html('<span style="color:red">' + data.error + '</span>');
@@ -101,7 +101,7 @@ $(document).ready(function () {
         /**
          * Get the unsigned transaction bytes
          */
-        if (step === 2) {
+        if (sendNxtStep === 2) {
 
             master_password = $("#tx_master_password").val();
 
@@ -122,7 +122,7 @@ $(document).ready(function () {
             };
 
             requestRunning = true;
-            $("#footer_message .loading").html('Please wait <img src="./../img/ajax-loader.gif">');
+            $("#footer_message .loading").html('<img src="./img/ajax-loader.gif">');
 
             $.get('api/send.php', data, function (data) {
                 requestRunning = false;
@@ -178,7 +178,7 @@ $(document).ready(function () {
                         $('#footer_message_button').html('<button type="button" class="btn btn-primary" data-dismiss="modal" id="ok">OK</button>');
                         $('#footer_message').show();
                         refresh_balance();
-                        step = 1;
+                        sendNxtStep = 1;
 
 
 
@@ -188,12 +188,131 @@ $(document).ready(function () {
         }
     }
 
+    var changePwdStep = 1, secret_phrase, salt_generated, new_master_salt;
+
+    /**
+     * Change the master password
+     */
+    function change_master_password() {
+        if (requestRunning) {
+            return;
+        }
+
+        requestRunning = true;
+        $(".loading").html('<img src="./img/ajax-loader.gif">').show();
+
+        if(changePwdStep === 1) {
+            var data = {
+                "getEncryptedWallet": 1
+            };
+
+            // get the encrypted wallet from the server
+            $.get('api/settings.php', data, function (data) {
+                requestRunning = false;
+                $(".loading").html('');
+                console.log(data);
+
+                if(typeof data.error !== "undefined") {
+                    // error handling
+                } else if(typeof data.encrypted_wallet !== "undefined" && typeof data.master_salt !== "undefined") {
+                    var account_id;
+
+                    secret_phrase = decryptMyNXTwallet(data.encrypted_wallet, data.master_salt, $("#tx_master_password_old").val());
+                    account_id = nxtCrypto.getAccountId(secret_phrase.substr(0,68));
+
+                    // compare decrypted secret phrase's account id with the own belonging to this account
+                    if(account_id === $("#tx_account_id").val()) {
+                        changePwdStep = 2;
+                        $("#change_password_body_main").hide();
+                        $("#change_password_footer_main").hide();
+                        $("#change_password_body_message").show();
+                        $("#change_password_footer_message").show();
+
+                        $(".change-master-password").addClass('disabled');
+
+                        sjcl.random = new sjcl.prng(8);
+                        sjcl.random.startCollectors();
+
+                        sjcl.random.addEventListener('progress', function(progress) {
+                            var progress = (sjcl.random.getProgress(8) * 100);
+
+                            $(".progress-bar").css('width', progress + '%').attr('aria-valuenow', progress).html(progress.toFixed(0) + "%");
+                        });
+
+                        sjcl.random.addEventListener('seeded', function() {
+                            salt_generated = true;
+                            sjcl.random.stopCollectors();
+
+                            var key_message = "<p><strong>We've generated a strong salt for you!</strong></p>";
+
+                            $("#key-generation").find(".help-block").html(key_message);
+                            $(".change-master-password").removeClass('disabled');
+                        });
+
+                    } else {
+                        $(".loading").html('<span style="color:red">Error decrypting your wallet. Please check your master password.</span>');
+                    }
+                }
+            });
+        }
+
+        if(changePwdStep === 2) {
+            var new_master_password1, new_master_password2;
+
+            new_master_salt = sjcl.random.randomWords(4,8);
+            new_master_salt = sjcl.codec.base64.fromBits(new_master_salt);
+            new_master_salt = strtr(new_master_salt, '+/=', '-_,');
+
+            new_master_password1 = $("#tx_master_password_new1").val();
+            new_master_password2 = $("#tx_master_password_new2").val();
+
+            var error = "";
+            if(new_master_password1.length < 8) {
+                error += '<li> The master password is too short</li>'
+            }
+            if(new_master_password1 !== new_master_password2) {
+                error += '<li> The master passwords are not the same</li>'
+            }
+
+            if (error) {
+                $('#msg_initial').hide()
+                $('#change_password_body_message').find('.msg-error').html(error).hide().show(300);
+                requestRunning = false;
+                $(".loading").html('');
+            } else {
+                var newly_encrypted_wallet = encryptMyNXTwallet(secret_phrase, new_master_salt, new_master_password1);
+                console.log(newly_encrypted_wallet);
+
+                var data = {
+                    "save_new_wallet": 1,
+                    "encrypted_wallet": newly_encrypted_wallet,
+                    "master_salt": new_master_salt
+                };
+                // store the new wallet
+                $.post('api/settings.php', data, function (data) {
+                    requestRunning = false;
+                    $(".loading").html('');
+
+                    if(data.status === "ok") {
+                        var footer_html = '<div class="col-xs-7 loading"></div>' +
+                            '<div class="col-xs-5"><button type="button" class="btn btn-primary" data-dismiss="modal" id="ok">OK</button></div>';
+
+                        $('#change_password_body_message').html("Successfully changed master password.").show();
+                        $('#change_password_footer_message').html(footer_html);
+                        changePwdStep = 1;
+                    }
+
+                });
+            }
+        }
+    }
+
     $("#sndModal").on('click', '.send-nxt', function () {
         send_nxt();
     });
 
     $('#sndModal').on('show.bs.modal', function (e) {
-        step = 1;
+        sendNxtStep = 1;
         $(".loading").html('');
         $("#body_main").show();
         $('#frmSend')[0].reset();
@@ -201,6 +320,24 @@ $(document).ready(function () {
         $("#footer_main").show();
         $('#body_message').hide();
         $('#footer_message').hide();
+    });
+
+    $("#changeMasterPasswordModal").on('click', '.change-master-password', function () {
+        change_master_password();
+    });
+
+    var changeMasterPasswordModalHtml = $("#changeMasterPasswordModal").html();
+
+    $('#changeMasterPasswordModal').on('show.bs.modal', function (e) {
+        changePwdStep = 1;
+        $(".loading").html('');
+
+        $("#changeMasterPasswordModal").html(changeMasterPasswordModalHtml);
+
+        $("#change_password_body_main").show();
+        $("#change_password_footer_main").show();
+        $('#change_password_body_message').hide();
+        $('#change_password_footer_message').hide();
     });
 
     $("#refresh").click(function () {
